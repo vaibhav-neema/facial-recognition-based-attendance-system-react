fea940cc309ac8d36483ff6e999ee075b996d99b
Shubh Laad <65905177+shubh-laad@users.noreply.github.com>
2023-07-28 19:12:43 +0530

    feature(project) : added basic react app structure (#11)

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import HomePage from "../src/pages/HomePage";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>
+);
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./index.scss";
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <div className="title">Smart Attendance System</div>
+      <div className="sub-title">
+        A System and Method for Taking Attendance in Educational and Professional Settings
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
