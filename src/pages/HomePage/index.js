bbab08b1dee0a4c4603b220e0020c777cfccdb4b
Shubh Laad <65905177+shubh-laad@users.noreply.github.com>
2023-07-30 17:10:48 +0530

    feature(project) : added complete homepage working code (#13)

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { loadModels } from "../../utils/loadedModels";
+
+import AppBar from "../../components/AppBar";
 
 import "./index.scss";
+import ImageCard from "../../components/ImageCard";
+import Separator from "../../components/Separator";
 
 const HomePage = () => {
+  useEffect(() => {
+    // load the models when homePage mounts
+    loadModels();
+  }, []);
+
   return (
-    <div className="container">
-      <div className="title">Smart Attendance System</div>
-      <div className="sub-title">
-        A System and Method for Taking Attendance in Educational and Professional Settings
+    <div id="root-container">
+      <AppBar />
+      <Separator />
+      <div className="image-container">
+        <ImageCard labelKey="upload" />
+      </div>
+
+      <div className="attendance-list">
+        <ol className="records"></ol>
       </div>
     </div>
   );
