402d62443a7c43b5911fae6cf4aa926d56251167
Shubh Laad <65905177+shubh-laad@users.noreply.github.com>
2023-08-11 00:27:44 +0530

    server(render) : updated api url (#23)

--- src/utils/enums.js
+++ src/utils/enums.js
@@ -1,1 +1,2 @@
-export const BASE_URI = "http://localhost:3001/student-data";
+// export const BASE_URI = "http://localhost:3001/student-data";
+export const BASE_URI = "https://facial-recognition-based-attendance-tecq.onrender.com/student-data";
