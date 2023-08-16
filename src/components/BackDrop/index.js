28d52df025af25adec11e463de5e334ca0796383
Shubh Laad <shubhlaad77@gmail.com>
2023-08-17 04:03:34 +0530

    feature(component) : added loader component and firebase files

--- src/components/BackDrop/index.js
+++ src/components/BackDrop/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Loader from "../Loader";
 import logo from "../../assets/images/davvlogo.png";
 
 import "./index.scss";
@@ -8,7 +9,8 @@ const BackDrop = () => {
   return (
     <div className="backdrop">
       <img src={logo} alt="loader-logo" />
-      <p>Loading Database...</p>
+      <Loader />
+      <p>Preparing System...</p>
     </div>
   );
 };
