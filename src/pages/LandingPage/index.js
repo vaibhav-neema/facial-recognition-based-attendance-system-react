018bee788a36a0a69d4ba5169f775bea236c5203
Divyanshu Parwal <68116519+DivyanshuParwal@users.noreply.github.com>
2023-08-18 23:17:23 +0530

    integrate landing page with home page (#28)

---
+++ src/pages/LandingPage/index.js
@@ -0,0 +1,143 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import clgLogo from "../../assets/images/ietdavv_logo.jpeg";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" align="center" {...props}>
+      {"Copyright © "}
+      <Link to="/">FaceIn</Link> {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignInSide() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      password: data.get("password"),
+    });
+    if (data.get("password") == "vneema") {
+      navigate("/home");
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              my: 7,
+            }}
+          >
+            <img src={clgLogo} />
+          </Box>
+          <Box
+            sx={{
+              mx: 4,
+              textAlign: "center",
+              p: 1,
+            }}
+          >
+            <p>
+              FaceIn revolutionizes traditional attendance tracking by
+              harnessing cutting-edge Facial Recognition Algorithms. By
+              extracting precise biometric data from photographs, this
+              innovative system seamlessly records classroom attendance. The
+              captured data is then cross-referenced against a meticulously
+              maintained database, instantly confirming students' class
+              participation. The output can be effortlessly generated as a
+              concise text file or a comprehensive Excel spreadsheet,
+              streamlining administrative tasks. Experience the future of
+              attendance management with FaceIn.
+            </p>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "#1b5e20" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              {/* <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              /> */}
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                color="success"
+                autoComplete="current-password"
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="success"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              {/* <Grid container> */}
+
+              {/* <Grid item>
+                  <Link to="/register">{"Don't have an account? Sign Up"}</Link>
+                </Grid> */}
+              {/* </Grid> */}
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
